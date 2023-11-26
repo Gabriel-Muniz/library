b1f59b717a250f896fcd3aab2b5b63da554d6863
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-11-26 00:28:27 -0300

    style make book visualization 3d

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const cardTemp = document.getElementsByTagName("template")[0];
+const clone = cardTemp.content.cloneNode(true);
+const container = document.querySelector(".content-container");
 const myLibrary = [];
 
 function Book(author, title, pages, readed) {
     this.author = author;
     this.title = title;
     this.pages = pages;
-    this.pages = readed;
+    this.readed = readed;
 }
+let book0 = new Book("Eu", "Meu Livro", 2, true);
 
-function addBookToLibrary() {
-
-}
+container.appendChild(clone);
