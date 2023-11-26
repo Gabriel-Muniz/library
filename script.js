da7427164aa4895fc24efe05545dd440ed60db82
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-11-25 21:05:43 -0300

    style basic layout

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("teste")
+const myLibrary = [];
+
+function Book(author, title, pages, readed) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.pages = readed;
+}
+
+function addBookToLibrary() {
+
+}
