fff5179e7acf0c8894462c6d0177d4a62defc31d
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-02 20:52:39 -0300

    :sparkles:FEAT: Add library and book object

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("It's working?");
+
+const myLibrary = [];
+
+function Book(title, author, pages, readed = false){
+  this.name = title;
+  this.author = author;
+  this.page = pages;
+  this.readed = readed;
+}
+
+Book.prototype.updateReadStatus = function(){
+  this.readed = !this.readed;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
